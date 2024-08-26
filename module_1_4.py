1fe22df675e9a4c918952fea0d5adf206fbc1b4e
SharapovaMarina <sanitzkaja.mar@yandex.ru>
2024-08-26 16:46:34 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Введите ваше имя: ")
+print(len(my_string))
+print("my_string".upper())
+print("my_string".lower())
+print("my string".replace("my string", "mystring"))
+name = "my_string"
+print(name[0])
+print(name[-1])
